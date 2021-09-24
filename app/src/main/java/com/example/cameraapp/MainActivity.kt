4e93bcffd28c4557dd1cbf70871d3bdcdd7d07a8
thisizankushk <ankushgautam510@gmail.com>
2021-09-24 11:06:02 +0530

    Second

--- app/src/main/java/com/example/cameraapp/MainActivity.kt
+++ app/src/main/java/com/example/cameraapp/MainActivity.kt
@@ -1,18 +1,24 @@
 package com.example.cameraapp
 
+import android.Manifest
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.BitmapFactory
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
+import android.widget.Toast
 import androidx.core.content.FileProvider
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
 
 val REQUEST_CODE = 100
 var FILE_NAME = "photo.jpg"
+private val IMAGE_PICK_CODE = 1000
+private val PERMISSION_CODE = 1001
 private lateinit var photoFile: File
 
 
@@ -24,6 +30,7 @@ class MainActivity : AppCompatActivity() {
         camerabutton.setOnClickListener {
 
             photoFile = getPhotoFile(FILE_NAME)
+            //intent for cameraApp
             val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
 
 
@@ -31,6 +38,50 @@ class MainActivity : AppCompatActivity() {
                 FileProvider.getUriForFile(this, "com.example.cameraapp.fileprovider", photoFile)
             takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider)
             startActivityForResult(takePictureIntent, REQUEST_CODE)
+        }
+
+        fetchbutton.setOnClickListener {
+            //checking the OS version of the phone
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                    //permission denied
+                    val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    requestPermissions(permissions, PERMISSION_CODE)
+                } else {
+                    pickImageFromGallery()
+                    //permission already granted
+                }
+            } else {
+                pickImageFromGallery()
+                //system os is >= marshmellow
+
+            }
+        }
+    }
+
+    private fun pickImageFromGallery() {
+        //intent for selecting image from gallery
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_PICK_CODE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            PERMISSION_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //permission granted from the popup
+                    pickImageFromGallery()
+                } else {
+                    //permission denied from the popup
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 
@@ -42,9 +93,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE) {
+            //setting image after chosing from the gallery
+            imageView.setImageURI(data?.data)
+        }
+
         if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
 
             val takenImage = BitmapFactory.decodeFile(photoFile.absolutePath)
+            //setting image after clicking
             imageView.setImageBitmap(takenImage)
         } else {
             super.onActivityResult(requestCode, resultCode, data)
@@ -52,4 +109,5 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+
 }
